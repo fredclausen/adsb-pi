5f22a4252ecd3782d95de9e18b3846faf7e199b8
Fred <43556888+fredclausen@users.noreply.github.com>
2021-04-09 18:07:14 -0600

    the install starter

---
+++ sdr-docker-config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#shellcheck disable=SC2128,SC1102
+command_line="$(printf %q "$BASH_SOURCE")$((($#)) && printf ' %q' "$@")"
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root! Try 'sudo $command_line'"
+   exit 1
+fi
+
+INSTALL_SCRIPT="https://raw.githubusercontent.com/fredclausen/sdr-docker-config/install-script/sdr-easy-install.sh"
+INSTALL_YAML="https://raw.githubusercontent.com/fredclausen/sdr-docker-config/install-script/sdr-docker-config.py"
+
+TERM=ansi whiptail --title "Working" --infobox "Downloading Install Files!" 8 78
+
+if [[ -e "sdr-easy-install.sh" ]]; then
+    rm sdr-easy-install.sh
+fi
+
+if [[ -e "sdr-easy-install.sh" ]]; then
+    rm sdr-docker-config.py
+fi
+
+wget "$INSTALL_SCRIPT" 2>&1 || exit
+wget "$INSTALL_YAML" 2>&1 || exit
+
+chmod +x sdr-easy-install.sh
+sudo ./sdr-easy-install.sh
